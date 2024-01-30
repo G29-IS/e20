4e1d97c9170ed33d4900c67286a9719b2dba8b6c
Marian Ologu <ologumarian2001@gmail.com>
2024-01-30 14:55:59 +0100

    Binded Firebase cloud services

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:e20/firebase_options.dart';
+
 import '/Screen/home_screen.dart';
 
-void main() {
+import '/Utils/console_log.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  logWarning('Initializing Firebase');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).onError((error, stackTrace) {
+    logError(error.toString());
+    return Future.error(error as Object);
+  });
+  logSuccess('Firebase initialized');
+
   runApp(const MyApp());
 }
 
