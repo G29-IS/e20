19fc9b75879923cd4a393f146a1f51ef9aec1b65
Marian Ologu <ologumarian2001@gmail.com>
2024-02-06 18:59:17 +0100

    REfactoring events dummy data

--- lib/Redux/Events/events_midlleware.dart
+++ lib/Redux/Events/events_midlleware.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:collection/collection.dart';
+import 'package:e20/models/event_place.dart';
 import 'package:redux/redux.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
@@ -20,24 +22,109 @@ List<Middleware<AppState>> createEventsMiddleware() {
 }
 
 _fetchEvents(Store<AppState> store, FetchEventsAction action, NextDispatcher next) async {
+  // store.dispatch(SetEventsLoadingStatusAction(LoadingStatus.loading));
+  // RequestHandler.fetchFeed().then((rawEvents) {
+  //   IList<Event> parsedEvents = rawEvents.map((element) => Event.fromMap(element)).toIList();
+
+  //   IMap<String, Event> events = {
+  //     for (var event in parsedEvents) event.idEvent: event,
+  //   }.lock;
+
+  //   IList<IMap<DateTime, IList<String>>> eventsFeed = [
+  //     (groupBy(parsedEvents, (event) {
+  //       return event.openingDateTime
+  //           .copyWith(hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
+  //     }).map((key, value) => MapEntry(key, value.map((e) => e.idEvent).toIList()))).toIMap()
+  //   ].lock;
+
+  //   store.dispatch(AddEventsToStateAction(events));
+  //   store.dispatch(AddEventsIdsToFeedAction(eventsFeed));
+
+  //   store.dispatch(SetEventsLoadingStatusAction(LoadingStatus.success));
+  // });
+
   store.dispatch(SetEventsLoadingStatusAction(LoadingStatus.loading));
-  RequestHandler.fetchFeed().then((rawEvents) {
-    IList<Event> parsedEvents = rawEvents.map((element) => Event.fromMap(element)).toIList();
 
-    IMap<String, Event> events = {
-      for (var event in parsedEvents) event.idEvent: event,
-    }.lock;
+  Future.delayed(
+    const Duration(seconds: 2),
+  );
 
-    IList<IMap<DateTime, IList<String>>> eventsFeed = [
-      (groupBy(parsedEvents, (event) {
-        return event.openingDateTime
-            .copyWith(hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
-      }).map((key, value) => MapEntry(key, value.map((e) => e.idEvent).toIList()))).toIMap()
-    ].lock;
+  /// PARSING
+  IMap<String, Event> events = Map<String, Event>.from({
+    ///DUMMY DATA
+    'idev_1': Event(
+      idEvent: 'idev_1',
+      name: '2024 Baby',
+      coverImageUrl:
+          "https://images.unsplash.com/photo-1704168370831-b7f450dadeb0?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      idOrganizer: 'idorg_1',
+      description: String.fromCharCodes(
+        Iterable.generate(100, (_) => 65 + Random().nextInt(26)),
+      ),
+      doorOpeningDateTime: DateTime.now().add(const Duration(days: 1)),
+      openingDateTime: DateTime.now().add(const Duration(days: 1, hours: 1)),
+      type: EventType.event,
+      visibility: EventVisibility.public,
+      availability: EventAvailability.available,
+      place: const EventPlace(
+        name: 'Piazza Duomo',
+        address: 'Piazza Duomo, Trento',
+      ),
+    ),
+    'idev_2': Event(
+      idEvent: 'idev_2',
+      name: 'Concerto',
+      coverImageUrl:
+          "https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      idOrganizer: 'idorg_2',
+      description: String.fromCharCodes(
+        Iterable.generate(100, (_) => 65 + Random().nextInt(26)),
+      ),
+      doorOpeningDateTime: DateTime.now().add(const Duration(days: 1)),
+      openingDateTime: DateTime.now().add(const Duration(days: 1, hours: 1)),
+      type: EventType.concert,
+      visibility: EventVisibility.public,
+      maxParticipants: 1500,
+      paymentLink:
+          "https://www.trento.info/dettaglio-evento/-/c/n/mercatino-di-natale-di-trento_a88d6e1b-8e03-5598-a1b9-0ea7c526b311",
+      availability: EventAvailability.running_out,
+      place: const EventPlace(
+        name: 'Piazza Fiera',
+        address: 'Piazza Fiera, Trento',
+      ),
+    ),
+    'idev_3': Event(
+      idEvent: 'idev_3',
+      name: 'AperiTrento',
+      coverImageUrl:
+          "https://images.unsplash.com/photo-1654171567840-58c77ee342d9?q=80&w=3610&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      idOrganizer: 'idorg_2',
+      description: String.fromCharCodes(
+        Iterable.generate(100, (_) => 65 + Random().nextInt(26)),
+      ),
+      doorOpeningDateTime: DateTime.now().add(const Duration(days: 2, hours: 4)),
+      openingDateTime: DateTime.now().add(const Duration(days: 2, hours: 5)),
+      type: EventType.bar,
+      visibility: EventVisibility.public,
+      availability: EventAvailability.running_out,
+      place: const EventPlace(
+        name: 'Baccus',
+        address: 'Piazza Fiera, 11 Trento',
+      ),
+    ),
+  }).toIMap();
 
-    store.dispatch(AddEventsToStateAction(events));
-    store.dispatch(AddEventsIdsToFeedAction(eventsFeed));
+  IList<IMap<DateTime, IList<String>>> eventsFeed = List<IMap<DateTime, IList<String>>>.from([
+    Map<DateTime, IList<String>>.from({
+      DateTime.now().add(const Duration(days: 1)): ['idev_1', 'idev_2'].lock,
+    }).toIMap(),
+    Map<DateTime, IList<String>>.from({
+      DateTime.now().add(const Duration(days: 2)): ['idev_3'].lock,
+    }).toIMap(),
+  ]).toIList();
 
-    store.dispatch(SetEventsLoadingStatusAction(LoadingStatus.success));
-  });
+  store.dispatch(AddEventsToStateAction(events));
+  store.dispatch(AddEventsIdsToFeedAction(eventsFeed));
+
+  store.dispatch(SetEventsLoadingStatusAction(LoadingStatus.success));
 }
